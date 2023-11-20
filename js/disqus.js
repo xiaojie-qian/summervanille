35cd027aaf691f588d0674f2ca4704e46721978c
Summervanille <xiaojie.qian@hhl.de>
2023-11-20 09:40:36 +0100

    Add files via upload

---
+++ js/disqus.js
@@ -0,0 +1,14 @@
+<div id="disqus_thread"></div>
+<script>
+    var disqus_config = function () {
+        this.page.url = window.location.href;
+        this.page.identifier = window.location.pathname;
+    };
+    
+    (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src =  'https://https-xiaojie-qian-github-io-summervanille.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+</script>
